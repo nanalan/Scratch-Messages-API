b668a45dcb9ec9e67c6b424c30d1c0abbbb0c4a3
Alex Bates <16batesa@gmail.com>
2016-07-28 21:05:51 +0100

    Better buildsystem

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,16 @@ const gulp   = require('gulp'),
       uglify = require('gulp-uglify'),
       rename = require('gulp-rename')
       pump   = require('pump')
-      
-gulp.task('default', [ 'build' ])
 
-gulp.task('build', () => {
+gulp.task('default', cb => {
   pump([
     gulp.src('main.js'),
     uglify(),
     rename('main.min.js'),
     gulp.dest(__dirname)
-  ])
+  ], cb)
+})
+
+gulp.task('watch', () => {
+  gulp.watch('main.js', ['default'])
 })
